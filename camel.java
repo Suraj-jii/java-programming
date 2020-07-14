33cb95ad81645488882e0b39e0f42875db35d3c6
Suraj-jii <sg110230@gmail.com>
2020-07-14 13:07:19 +0530

    user input

---
+++ camel.java
@@ -0,0 +1,11 @@
+class add
+{
+   public static void main(String args[])
+{
+   int a,b,c;
+   a=Integer.parseInt(args[0]);
+   b=Integer.parseInt(args[1]);
+   c=a+b;
+   System.out.println("the summation is="+c);
+}
+}
